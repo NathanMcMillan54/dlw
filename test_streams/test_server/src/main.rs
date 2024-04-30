38c583fba4ebd97c26d78bc9d2708c3ba588e20a
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2024-04-30 19:42:34 +0000

    Changed test_server example.

--- test_streams/test_server/src/main.rs
+++ test_streams/test_server/src/main.rs
@@ -1,31 +1,9 @@
-use dlwp::codes::{Code, STATUS_OK};
+use dlwp::codes::{Code, REQUEST_RESPONSE, STATUS_OK};
 use dlwp::message::ReceiveInfo;
 use dlwp::stream::{Stream, StreamType};
 use std::{thread::sleep, time::Duration};
 
 const PORT: u16 = 5000;
-
-fn handle_client(tid: u64, tdid: u32) {
-    let mut client = Stream::new(
-        StreamType::Client {
-            rid: tid,
-            rdid: tdid,
-            port: PORT,
-        },
-        false,
-    );
-
-    println!("handling....");
-
-    client.start();
-    sleep(Duration::from_millis(500));
-
-    client.write(String::from("serving stuff"), STATUS_OK);
-    sleep(Duration::from_millis(100));
-    client.stop();
-
-    return;
-}
 
 fn main() {
     let mut stream = Stream::new(StreamType::Server { port: PORT }, false);
@@ -39,14 +17,11 @@ fn main() {
         let read = stream.read();
         println!("{}", read.len());
         for r in read {
-            println!("{:?}", Code::new(r.ti.code));
-            handle_client(r.ti.tid, r.ti.tdid);
-            stream.remove_connection(ReceiveInfo {
-                rid: r.ti.tid,
-                rdid: r.ti.tdid,
-                instance_id: r.ri.instance_id,
-                port: r.ri.port,
-            });
+            if !stream.check_add_connection(r) {
+                continue;
+            }
+
+            stream.server_write(r.ti, String::from("wsg?"), REQUEST_RESPONSE);
         }
 
         sleep(Duration::from_millis(80));
