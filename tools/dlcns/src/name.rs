0e27a6a513a4dca92a789145fea0c94f6131b335
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2024-05-03 15:16:59 +0000

    Added empty function to NamesList.

--- tools/dlcns/src/name.rs
+++ tools/dlcns/src/name.rs
@@ -26,6 +26,12 @@ pub struct NamesList {
 }
 
 impl NamesList {
+    pub const fn empty() -> Self {
+        return NamesList {
+            list: vec![],
+        };
+    }
+
     pub fn from_file(file: String) -> Option<Self> {
         let read = read_to_string(file);
         if read.is_err() {
