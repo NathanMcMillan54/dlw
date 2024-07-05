b685f34350327bd46872603eeeebdf6fc90fa8b8
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2024-07-05 19:12:28 +0000

    Added proper fields to tools/dlcns/Cargo.toml for publishing.

--- tools/dlcns/src/lib.rs
+++ tools/dlcns/src/lib.rs
@@ -6,7 +6,7 @@ use dlwp::id::{DId, LId, Port};
 pub mod get;
 pub mod name;
 
-pub const CNS_ID: LId = 505051114;
+pub const CNS_ID: LId = 1000;
 pub const CNS_PORT: Port = 4999;
 pub const CNS_DISTRIBUTOR: DId = 3;
 pub const OWNERS_LIST: &str = "owners_list.json";
