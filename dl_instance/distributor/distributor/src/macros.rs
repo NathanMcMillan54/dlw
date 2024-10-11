3f372d2e05b3e81f941d5135300ba2ac28f2a115
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2024-10-11 02:52:08 +0000

    Wrote error handler for io function calls in distributor (lib).

--- dl_instance/distributor/distributor/src/macros.rs
+++ dl_instance/distributor/distributor/src/macros.rs
@@ -11,10 +11,53 @@ macro_rules! sleep_condition {
     }
 }
 
-/// Handles errors that come from ``std::io::Result``
+/// Check if there was an error while calling an ``std::io`` function. If this macro returns ``true`` then the socket/stream
+/// should be disconnected, it will return ``false`` if ``TimedOut`` or ``WouldBlock`` is returned from the function.
+/// 
+/// ``($code:expr)`` and ``($code:expr, $log:expr)`` run the same code except ``($code:expr, $log:expr)`` is intended to log
+/// any information if an error comes up.
 #[macro_export]
-macro_rules! io_handle {
-    () => {
-        unimplemented!()
+macro_rules! io_err_check {
+    ($code:expr) => {
+        {
+            let tmp_ret = $code;
+
+            if tmp_ret.is_err() {
+                match tmp_ret.err().unwrap().kind() {
+                    std::io::ErrorKind::NotConnected | std::io::ErrorKind::BrokenPipe | std::io::ErrorKind::ConnectionReset | std::io::ErrorKind::ConnectionRefused | std::io::ErrorKind::ConnectionAborted => {
+                        true
+                    },
+                    std::io::ErrorKind::TimedOut | std::io::ErrorKind::WouldBlock => false,
+                    _ => {
+                        true
+                    },
+                }
+            } else {
+                tmp_ret.unwrap();
+                false
+            }
+        }
+    };
+
+    ($code:expr, $log:expr) => {
+        {
+            let tmp_ret = $code;
+
+            if tmp_ret.is_err() {
+                match tmp_ret.err().unwrap().kind() {
+                    std::io::ErrorKind::NotConnected | std::io::ErrorKind::BrokenPipe | std::io::ErrorKind::ConnectionReset | std::io::ErrorKind::ConnectionRefused | std::io::ErrorKind::ConnectionAborted => {
+                        $log;
+                        true
+                    },
+                    std::io::ErrorKind::TimedOut | std::io::ErrorKind::WouldBlock => false,
+                    _ => {
+                        true
+                    },
+                }
+            } else {
+                tmp_ret.unwrap();
+                false
+            }
+        }
     };
 }
